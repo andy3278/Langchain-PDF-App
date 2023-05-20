0e511dc97c646f3e7f26ac9dc9a386b637e35193
Andy Cheung <77382250+andy3278@users.noreply.github.com>
2023-05-20 17:05:50 +0800

    updated app

---
+++ app.py
@@ -0,0 +1,17 @@
+from dotenv import load_dotenv
+import streamlit as st
+
+
+
+def main ():
+    # load env secret
+    load_dotenv()
+    
+    st.set_page_config(page_title = "Ask PDF")
+    st.header("Ask PDF")
+
+    pdf = st.file_uploader("Upload your PDF here", type = ["pdf"])
+
+
+if __name__ == '__main__':
+    main()
