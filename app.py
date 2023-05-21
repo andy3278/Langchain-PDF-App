b922a16663dfd0051d764c653edd23fb54a9a3b5
Andy Cheung <shcheungai@connect.ust.hk>
2023-05-21 15:45:18 +0800

    update code

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from dotenv import load_dotenv
 import streamlit as st
-
-
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
 
 def main ():
     # load env secret
@@ -9,9 +11,36 @@ def main ():
     
     st.set_page_config(page_title = "Ask PDF")
     st.header("Ask PDF")
-
+    # upload pdf
     pdf = st.file_uploader("Upload your PDF here", type = ["pdf"])
 
+    # extract text from pdf
+    if pdf is not None:
+        pdf_reader = PdfReader(pdf)
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+
+        # split text into chunks\
+        splitter = CharacterTextSplitter(
+            separator= "\n",
+            chunk_size = 1000,
+            chunk_overlap  = 200,
+            length_function = len
+        )
+        chunks = splitter.split_text(text=text)
+
+        # st.write(chunks)
+        
+        # create embeddings 
+        embeddings = OpenAIEmbeddings()
+        db = FAISS.from_texts(chunks, embedding=embeddings)
+
+        question = st.text_input("input a question about the pdf: ")
+        if question is not None:
+            # find top k relevant docs 
+            docs = db.similarity_search(question, k = 4)
+            st.write(docs)
 
 if __name__ == '__main__':
     main()
